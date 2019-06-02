09845ff557c082e643bdea14e28e9a9471cbc745
Fabiola <fabiolaalvarado11@gmail.com>
2019-06-01 18:24:42 -0600

    Clase homePage capturar mensaje tweet en proceso

--- app/src/main/java/com/example/twitter3/HomePage.java
+++ app/src/main/java/com/example/twitter3/HomePage.java
@@ -6,6 +6,8 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.MultiAutoCompleteTextView;
 import android.widget.Toast;
 
 import com.twitter.sdk.android.core.TwitterCore;
@@ -18,25 +20,22 @@ public class HomePage extends AppCompatActivity {
     private TabLayout tabLayout;
 
     private ViewPager viewPager;
-
+    private EditText multiText;
 
     public void goFinalActivity(View view) {/// Método que me envía o redirige hacia una vista que maneja una clase
         Intent intent = new Intent( this, HomePage.class);
         startActivity(intent);
     }
-
     public void tweets(View view) {/// Método que me envía o redirige hacia una vista que maneja una clase
         final TwitterSession session = TwitterCore.getInstance().getSessionManager()
                 .getActiveSession();
         final Intent intent = new ComposerActivity.Builder(this)
                 .session(session)
-                .text(String.valueOf(R.id.button3))
+                .text(String.valueOf(multiText.getText()))
                 .hashtags("#twitter")
                 .createIntent();
         startActivity(intent);
     }
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +44,8 @@ public class HomePage extends AppCompatActivity {
         tabLayout=(TabLayout)findViewById(R.id.tabLayoutId);// asocio con el id que se le da en la vista del xml
 
         viewPager= (ViewPager)findViewById(R.id.viewPagerId);
+        multiText= (EditText) findViewById(R.id.multiText);
+
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());// Me falta de entender bien este metodo
         //aca se llama al metodo addFragment creado en la clase ViewPageAdapters donde se añaden los Fragmentos/Tabs
         adapter.addFragments(new UserTimelineFragment(),"TIMELINE");
